d890da6a4db7c2580f1a508b552ba1f8a82f0443
md-sofiul-alam <sowrobeakter@gmail.com>
2023-05-11 23:22:24 +0600

    Header is added

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
+import { RouterProvider } from 'react-router-dom';
+import router from './components/routes/Routes.jsx';
+import AuthProviders from './components/AuthProviders/AuthProviders.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+   
   </React.StrictMode>,
 )
